65b9b6e7466eda894864a4a2076df98993d35b3b
Bardia Shafaee <bardia.shafaee@outlook.com>
2023-08-02 11:19:32 -0700

    Initial Commit

---
+++ SchoolConnect/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SchoolConnect
+//
+//  Created by Bardia Shafaee on 8/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SchoolConnect/SchoolConnectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolConnectApp.swift
+//  SchoolConnect
+//
+//  Created by Bardia Shafaee on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct SchoolConnectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
